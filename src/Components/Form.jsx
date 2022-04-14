3bf3850b5415a5682a849ae606ef3343bf225010
melekabbassi <abbassimelek@gmail.com>
2022-04-14 13:09:48 +0100

    fixed dispaly & nav

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,9 +1,75 @@
 import React from 'react'
+import {useState} from 'react'
 
-const Form = () => {
+const Form = (props) => {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [genre, setGenre] = useState('');
+    const [image, setImage] = useState('');
+    
   return (
-    <div>Form</div>
-  )
+    <div className="formDiv">
+    <form>
+        <h1>Add a new Movie</h1>
+        <div className="formGroup">
+            <label htmlFor="title">Title</label>
+            <input 
+                onChange={(e)=>setTitle(e.target.value)} 
+                type="text" 
+                className="form-control" 
+                id="title" 
+                placeholder="Enter Title" 
+            />
+        </div>
+        <div className="formGroup">
+            <label htmlFor="description">Description</label>
+            <input
+                onChange={(e)=>setDescription(e.target.value)} 
+                type="text" 
+                className="form-control" 
+                id="description" 
+                placeholder="Enter Description" 
+            />
+        </div>
+        <div className="formGroup">
+            <label htmlFor="genre">Genre</label>
+            <input 
+                onChange={(e)=>setGenre(e.target.value)}
+                type="text" 
+                className="form-control" 
+                id="genre" 
+                placeholder="Enter Genre" 
+            />
+        </div>
+        <div className="formGroup">
+            <label htmlFor="image">Image</label>
+            <input 
+                onChange={(e)=>setImage(e.target.value)}
+                type="file" 
+                className="formControl" 
+                id="image" 
+                placeholder="Enter Image" 
+            />
+        </div>
+        <button 
+            onClick={(e)=>{
+                console.log(props)
+                e.preventDefault();
+                props.addMovie(title, description, genre, image)
+                setTitle('');
+                setDescription('');
+                setGenre('');
+                setImage('');
+            }}
+            type="submit" 
+            className="btn btn-primary"
+            >
+            Submit
+            </button>
+    </form>
+    </div>
+    )
 }
 
 export default Form
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
 import Popcorn from '../Images/popcorn.svg'
 import { Link } from 'react-router-dom'
-import FavoriteContext from '../FavoriteContext'
-import { useContext } from 'react'
+// import FavoriteContext from '../FavoriteContext'
+// import { useContext } from 'react'
 
 export const Nav = () => {
-    const {items} = useContext(FavoriteContext);
+    // const {items} = useContext(FavoriteContext);
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <Link to={"/"}>
-            <Popcorn />
-            <span>{items.length}</span>
+            <img src={Popcorn} alt="" className='logo'/>
+            {/* <span>{items.length}</span> */}
         </Link>
-        <Link to={"/"}>
+        <Link to={"/"} className="navbar-items">
             <h1>Home</h1>
         </Link>
-        <Link to={"/Favorite"}>
+        <Link to={"/Favorite"} className="navbar-items">
             <h1>Favorite</h1>
         </Link>
-        <Link to={"/Form"}>
+        <Link to={"/Form"} className="navbar-items">
             <h1>Form</h1>
         </Link>
+
+        <input className="search" type="text" placeholder="Search" />
     </nav>
 
   )
