e1547e1de09f04421325efb5ea97effa6fbdec7b
Melek <abbassimelek@gmail.com>
2022-04-17 15:42:07 +0100

    added some movies

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -10,7 +10,10 @@ const Movies = () => {
         {title: "The Dark Knight", year: 2008},
         {title: "12 Angry redmen", year: 1957},
         {title: "Schindler's List", year: 1993},
-        {title: "Pulp Fiction", year: 1994},                   
+        {title: "Pulp Fiction", year: 1994},
+        {title: "The Good, the Bad and the Ugly", year: 1966},
+        {title: "The Lord of the Rings: The Fellowship of the Ring", year: 2001},
+        {title: "The Lord of the Rings: The Two Towers", year: 2002},
     ];
 
   return (
@@ -18,7 +21,6 @@ const Movies = () => {
         {movies.map((movie) => (
             <Card title={movie.title} year={movie.year} />
         ))}
-        )
     </div>
   )
 }
